d0795f4de799cfac1b5df1c6d24f060a509b8398
RLews <410670107@qq.com>
2021-08-21 11:20:26 +0800

    1.更新生成运行文件脚本。 2.raspberry pi4b运行验证。

--- 03_Project_Code/gen_update_package.sh
+++ 03_Project_Code/gen_update_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "FAW-TBOX-SDK-Package-Generate Start."
+echo "Package-Generate Start."
 
 # export PATH=$PATH:/CompileChain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabi/bin
 # 可执行文件复制路径
@@ -7,11 +7,13 @@ CopyExePath="/mnt/hgfs/Lews/Tools/FTP_Path"
 
 cd ./x_platform || exit
 
+make clean
+
 make
 wait
 
-cp ./bin/x_platform $CopyExePath || exit
-wait
-echo -e "\033[32m copy to FTP_Path success. \033[0m"
+#cp ./bin/x_platform $CopyExePath || exit
+#wait
+#echo -e "\033[32m copy to FTP_Path success. \033[0m"
 
 echo -e "\033[32m generate completed. \033[0m"
